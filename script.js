dc8763aa4b3dd9c9acf241bcf9d4de0c25a83ec6
Maria james <mariaajames280@gmail.com>
2026-02-09 19:51:14 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    // Remove active class from all dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    // Show current slide and activate corresponding dot
+    slides[slideIndex - 1].style.display = "block";
+    if (dots.length > 0) {
+        dots[slideIndex - 1].className += " active";
+    }
+}
+
+// Auto-advance slides every 5 seconds
+let slideInterval = setInterval(() => {
+    plusSlides(1);
+}, 5000);
+
+// Pause auto-advance on hover
+let slideshowContainer = document.querySelector('.slideshow-container');
+if (slideshowContainer) {
+    slideshowContainer.addEventListener('mouseenter', () => {
+        clearInterval(slideInterval);
+    });
+    
+    slideshowContainer.addEventListener('mouseleave', () => {
+        slideInterval = setInterval(() => {
+            plusSlides(1);
+        }, 5000);
+    });
+}
